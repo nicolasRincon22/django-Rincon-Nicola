44390c6bd0f341ba056944f1735b2c5a2a7f2aad
Your Name <nicolas.rinconmarin@alessandrinimainardi.edu.it>
2021-11-04 11:32:52 +0100

    "Livello 5"

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from prima_app.views import homepage,welcome,lista,chi_siamo,variabili
+from prima_app.views import homepage,welcome,lista,chi_siamo,variabili,index
 
 app_name="prima_app"
 urlpatterns=[
-    path('', homepage, name='homepage'),
+    path('homepage', homepage, name='homepage'),
      path('welcome', welcome, name='welcome'),
      path('lista', lista, name='lista'),
      path('chi_siamo', chi_siamo, name='chi_siamo'),
-     path("variabili", variabili, name='variabili')
+     path("variabili", variabili, name='variabili'),
+     path("", index, name='index'),
 
     
     
--- prima_app/views.py
+++ prima_app/views.py
@@ -16,3 +16,5 @@ def variabili(request):
         'var3': 'Terza variabile',
     }
     return render(request, "variabili.html",comtext)
+def  index(request):
+    return render(request,"index.html")
