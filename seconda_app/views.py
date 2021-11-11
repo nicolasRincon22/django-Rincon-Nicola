392e5ada26ec266c2b8d14a1b2b2bea45e6f8081
Your Name <nicolas.rinconmarin@alessandrinimainardi.edu.it>
2021-11-11 10:43:40 +0100

    "Livello 7"

--- seconda_app/views.py
+++ seconda_app/views.py
@@ -8,3 +8,10 @@ def es_if(request) :
         'var3' : 300
     }
     return render(request, "es_if.html", context)
+def if_else_elif(request):
+    context = {
+        'var1' : 100,
+        'var2' : 100.0,
+        'var3' : 100.50,
+    }
+    return render(request, "if_else_elif.html", context)
