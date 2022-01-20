dae498988c3bd760dab56f0b6450f229edc35673
nicolasRincon22 <92153310+nicolasRincon22@users.noreply.github.com>
2022-01-20 12:03:17 +0100

    prova pratica 1_3

--- prova_pratica_1/views.py
+++ prova_pratica_1/views.py
@@ -12,5 +12,5 @@ def view_c(request):
             'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9.5,0),("Storia",8,2),("Geografia",8,1)],
             'Nicola Spina':[("Matematica",7.5,2),("Italiano",6,2),("Inglese",4,3),("Storia",8.5,2),("Geografia",8,2)]}
     }
-return render(request, "view_c.html", context)
+    return render(request, "view_c.html", context)
 # Create your views here.
