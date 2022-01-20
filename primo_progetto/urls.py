6caacfac882d13db5c086c3772d97bc2ff0d34ea
nicolasRincon22 <92153310+nicolasRincon22@users.noreply.github.com>
2022-01-20 12:01:42 +0100

    prova pratica 1_2

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("prima_app.urls",namespace="prima_app")),
     path('seconda_app/', include('seconda_app.urls',namespace="seconda.app")),
-    path('news/', include('news.urls',namespace="news"))
+    path('news/', include('news.urls',namespace="news")),
     path('prova_pratica_1/', include('prova_pratica_1.urls',namespace="prova_pratica_1"))
 ]
